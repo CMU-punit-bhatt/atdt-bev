6058c1daa7fc9d35d843d230175ad4f73cf94752
adithyasampath <sampath.adithya96@gmail.com>
2022-04-25 07:57:27 +0000

    Fixed visualization to visualize output from model

--- visualize.py
+++ visualize.py
@@ -4,7 +4,15 @@ import torchvision
 import matplotlib as plt
 from PIL import Image
 import os
+import utils
+from wandb import visualize
 from carla_nuscenes_map import CARLA_CLASSES_NAME_TO_RGB
+import dataloaders.dataloader as dataloader
+import cv2
+import hydra
+from collections import OrderedDict
+from omegaconf import DictConfig
+from models import get_network
 
 # reference: https://datascience.stackexchange.com/questions/40637/how-to-visualize-image-segmentation-results
 
@@ -15,26 +23,27 @@ def visualise(model, input, save_path, channels_dim=1):
     input: input image to run inference
     save_path: list of save paths to each of the output image
     '''
+
     output = model(input.cuda())
+    output = output["out"]
     
     # assuming output shape of B x N x H x W
     segmented_image = np.zeros((output.shape[0], output.shape[-2], output.shape[-1], 3))  
-    
+
     # assuming N channels in dim=1
-    output = torch.amax(output, dim=channels_dim)
+    output = torch.argmax(output, dim=channels_dim)
     output = output.detach().cpu().numpy()
-    
+
     for key in CARLA_CLASSES_NAME_TO_RGB:
         segmented_image[output==key] = CARLA_CLASSES_NAME_TO_RGB[key]
-    
+
     for i, path in enumerate(save_path):
         out_img = segmented_image[i]
         out_img = out_img.astype(np.uint8)
         out_img = Image.fromarray(out_img).convert('RGB')
         out_img.save(path)
 
-# Unit Test script
-if __name__ == "__main__":
+def unit_test1():
     batch_image = None
     save_path = []
     model = lambda x: x
@@ -53,5 +62,34 @@ if __name__ == "__main__":
         save_path.append(path)
         
     visualise(model, batch_image, save_path)
+  
+
+@hydra.main(config_path='./configs', config_name='train_front')
+def main(cfg: DictConfig):
+    params = cfg.training
+    ckpt_filename = "/home/adithyas/atdt/n1_model_best.pt"
+    model = get_network(params).to(torch.device("cuda"))
+    best_value = -float('inf')
+    model, _, _, _, best_value = utils.load_checkpoint(model,
+                                  None,
+                                  None,
+                                  0,
+                                  False,
+                                  best_value,
+                                  ".",
+                                  ckpt_filename)
+    model.eval()
+    img_path = "/home/adithyas/atdt/002180_rgb.png"
+    img = cv2.imread(img_path)
+    img = Image.fromarray(img).convert('RGB')
+    toTensor = torchvision.transforms.ToTensor()
+    img = toTensor(img)
+    input = img.unsqueeze(0).cuda()
+    save_path = ["/home/adithyas/atdt/002180_result.png"]
+    visualise(model, input, save_path)
+
+if __name__ == '__main__':
+    main()
+
     
     
